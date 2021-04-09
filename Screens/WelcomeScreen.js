2b8ebe9208831a8278bec5d0e7c2fb45ed923880
NaNo <66743594+ANiBiNGi@users.noreply.github.com>
2021-04-09 15:01:35 -0500

    Add files via upload

---
+++ Screens/WelcomeScreen.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert, Modal, ScrollView, KeyboardAvoidingView } from 'react-native';
+import SantaAnimation from '../components/SantaClaus.js';
+import db from '../config';
+import firebase from 'firebase';
+
+export default class WelcomeScreen extends Component {
+  constructor(){
+    super()
+    this.state={
+      emailId : '',
+      password: '',
+      isModalVisible: "false",
+      firstName:"",
+      LastName:"",
+      address:"",
+      contact:"",
+      confirmPassword:"",
+    }
+  }
+
+  userLogin = (emailId, password)=>{
+    firebase.auth().signInWithEmailAndPassword(emailId, password)
+    .then(()=>{
+      return Alert.alert("Successfully Logged in")
+    })
+    .catch((error)=> {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage)
+    })
+  }
+
+  userSignUp = (emailId, password, confirmPassword) =>{
+    if(password!==confirmPassword){
+      return Alert.alert("password doesn't match")
+    }
+    else{
+      firebase.auth().createUserWithEmailAndPassword(emailId, password)
+    .then(()=>{
+      db.collection('users').add({
+        first_name:this.state.firstName,
+        last_name:this.state.lastName,
+        mobile_number:this.state.contact,
+        user_name:this.state.emailID,
+        address:this.state.address
+      })
+      return Alert.alert("User Added Successfully")
+    })
+    
+    
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      return Alert.alert(errorMessage)
+    });
+  }
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <View style={styles.profileContainer}>
+          {/*<SantaAnimation/>*/}
+          <Text style={styles.title}>Barter</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TextInput
+          style={styles.loginBox}
+          placeholder="example@booksanta.com"
+          placeholderTextColor = "#ffff"
+          keyboardType ='email-address'
+          onChangeText={(text)=>{
+            this.setState({
+              emailId: text
+            })
+          }}
+        />
+
+        <TextInput
+          style={styles.loginBox}
+          secureTextEntry = {true}
+          placeholder="password"
+          placeholderTextColor = "#ffff"
+          onChangeText={(text)=>{
+            this.setState({
+              password: text
+            })
+          }}
+        />
+          <TouchableOpacity
+            style={[styles.button,{marginBottom:20, marginTop:20}]}
+            onPress = {()=>{this.userLogin(this.state.emailId, this.state.password)}}
+            >
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={()=>{this.userSignUp(this.state.emailId, this.state.password)}}
+            >
+            <Text style={styles.buttonText}>SignUp</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#F8BE85'
+  },
+  profileContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  title :{
+    fontSize:70,
+    fontWeight:'350',
+    paddingBottom:35,
+    color : '#ff3d00'
+  },
+  loginBox:{
+    width: 300,
+    height: 40,
+    borderBottomWidth: 1.5,
+    borderColor : '#ff8a65',
+    fontSize: 20,
+    margin:10,
+    paddingLeft:10
+  },
+  button:{
+    width:300,
+    height:50,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:25,
+    backgroundColor:"#ff9800",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 10.32,
+    elevation: 16,
+  },
+  buttonText:{
+    color:'#ffff',
+    fontWeight:'200',
+    fontSize:20
+  },
+  buttonContainer:{
+    flex:1,
+    alignItems:'center'
+  }
+})
